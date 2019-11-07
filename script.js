febd328be8ab60c409d215445bc4a6f4198ae190
Thao Truong <thaotruong@Thaos-iMac.local>
2019-11-07 11:50:01 +0000

    post update func works

--- script.js
+++ script.js
@@ -6,60 +6,50 @@ var postImgUrl = document.getElementById("post-img");
 var postLinkUrl = document.getElementById("post-link");
 // var previewImg = document.getElementById("preview-img");
 //get input elements
-var titleInput = document.getElementById("title-input").value;
-var linkInput = document.getElementById("link-input").value;
+var titleInput = document.getElementById("title-input");
+var linkInput = document.getElementById("link-input");
+var endDateInput = document.getElementById("endDate-input");
 var uploader = document.getElementById("uploader");
 var fileButton = document.getElementById("fileButton"); //for display progress
 var submitPost = document.getElementById("submit-post-btn");
 
-//get the post from database
+//get the post from database to show on home page
 if (postTitle !== null && postImgUrl !== null && postLinkUrl !== null) {
   getSocialPost();
 }
-var file, imgUrl;
+var file, imgUrl, titleVal, linkVal, endDateVal;
 if (fileButton !== null) {
   fileButton.addEventListener("change", function(e) {
     file = e.target.files[0];
     console.log(file);
   });
 }
-
+if (titleInput !== null) {
+  titleInput.addEventListener("change", function(e) {
+    titleVal = e.target.value;
+    console.log("titleVal", titleVal);
+  });
+}
+if (linkInput !== null) {
+  linkInput.addEventListener("change", function(e) {
+    linkVal = e.target.value;
+    console.log("linkVal", linkVal);
+  });
+}
+if (endDateInput !== null) {
+  endDateInput.addEventListener("change", function(e) {
+    //covert string to date obj then timestamp
+    endDateVal = new Date(e.target.value);
+  });
+}
 //submit post form
-submitPost.addEventListener("click", function() {
-  // e.preventDefault();
-  //upload file to storage
-  var storage = firebase.storage();
-  // Create a storage ref
-  var storageRef = firebase.storage().ref("SocialPost/" + file.name);
-  // Upload file
-  var task = storageRef.put(file);
-  // Update progress bar
-  task.on(
-    "state_changed",
-    function progress(snapshot) {
-      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      uploader.value = percentage;
-    },
-    function error(err) {},
-    function complete() {
-      task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        // previewImg.innerHTML = '<img src="' + downloadURL + '"/>';
-        imgUrl = downloadURL;
-        console.log("Img url", imgUrl);
-        //updateSocialPost(imgUrl, titleInput, linkInput);
-      });
-    }
-  );
-});
-
-// Listen for file selection
-if (fileButton !== null) {
-  fileButton.addEventListener("change", function(e) {
+if (submitPost !== null) {
+  submitPost.addEventListener("click", function() {
+    // e.preventDefault();
+    //upload file to storage
     var storage = firebase.storage();
-    // Get file
-    var file = e.target.files[0];
     // Create a storage ref
-    var storageRef = firebase.storage().ref("SocialPost/" + file.name);
+    var storageRef = firebase.storage().ref("SocialPost/postImg.jpg");
     // Upload file
     var task = storageRef.put(file);
     // Update progress bar
@@ -74,8 +64,9 @@ if (fileButton !== null) {
       function complete() {
         task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
           // previewImg.innerHTML = '<img src="' + downloadURL + '"/>';
-
-          console.log("Img url", downloadURL);
+          imgUrl = downloadURL;
+          console.log("Img url", imgUrl);
+          updateSocialPost(imgUrl, titleVal, linkVal, endDateVal);
         });
       }
     );
@@ -117,8 +108,17 @@ function getSocialPost() {
     });
 }
 
-function updateSocialPost(imgUrl, title, linkUrl) {
+function updateSocialPost(imgUrl, title, linkUrl, endDate) {
   var db = firebase.firestore();
   var firstPostDoc = db.collection("socialPost").doc("firstPost");
-  firstPostDoc.set({ imgUrl, title, linkUrl }, { merge: true });
+  firstPostDoc.set({ imgUrl }, { merge: true });
+  if (title !== null && title !== "") {
+    firstPostDoc.set({ title }, { merge: true });
+  }
+  if (linkUrl !== null && linkUrl !== "") {
+    firstPostDoc.set({ linkUrl }, { merge: true });
+  }
+  if (endDate !== null && endDate !== "") {
+    firstPostDoc.set({ endDate }, { merge: true });
+  }
 }
